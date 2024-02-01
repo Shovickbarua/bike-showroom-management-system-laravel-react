64a92738b166d88673802b3c39e03bb34d1eeaba
Shovickbarua <shovickbarua92@gmail.com>
2024-02-02 00:25:23 +0600

    update

--- resources/js/pages/products/Product.jsx
+++ resources/js/pages/products/Product.jsx
@@ -1,41 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ProductServices from "../../services/ProductServices";
 
 const Product = () => {
-  return (
-    <div className="row">
-			<div className="col-lg-12">
-				<div className="card">
-				  <div className="card-header">
-					<h3 className="card-title">All Product</h3>
-					<Link to="/product/add" className="btn btn-success float-right btn-sm"><i className="fa fa-plus-circle"></i>Add Product</Link>
-				  </div>
-					{/* <!-- /.card-header --> */}
-					<div className="card-body">
-          <table id="example" className="table table-bordered table-striped">
-						  <thead>
-						  <tr>
-                <th width="7%">Serial</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>SKU</th>
-                <th>Quantity</th>
-                <th>Date</th>
-                <th>Image</th>
-                <th width="">Action</th>
-                <th width=""></th>
-						  </tr>
-						  </thead>
-						  <tbody>
-							{/* @foreach($products as $product)
-								<tr>
-									<td>{{$loop->iteration}}</td>
-									<td>{{$product->product_name}}</td>
-									<td>{{$product->cat_name}}</td>
-									<td>{{$product->SKU}}</td>
-									<td>{{$product->quantity}}</td>
-									<td>{{$product->dob}}</td>
-									<td><img src="{{asset('/products/'.$product->image)}}" style="height:150px; width:150px;"/></td>
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        getProduct();
+    }, []);
+    const getProduct = async () => {
+        const res = await ProductServices.index();
+        console.log("data", res);
+        if (res.success) {
+            setProduct(res.data);
+        }
+    };
+    return (
+        <div className="row">
+            <div className="col-lg-12">
+                <div className="card">
+                    <div className="card-header">
+                        <h3 className="card-title">All Product</h3>
+                        <Link
+                            to="/product/add"
+                            className="btn btn-success float-right btn-sm"
+                        >
+                            <i className="fa fa-plus-circle"></i>Add Product
+                        </Link>
+                    </div>
+                    {/* <!-- /.card-header --> */}
+                    <div className="card-body">
+                        <table
+                            id="example"
+                            className="table table-bordered table-striped"
+                        >
+                            <thead>
+                                <tr>
+                                    <th width="7%">Serial</th>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>SKU</th>
+                                    <th>Quantity</th>
+                                    <th>Date</th>
+                                    <th>Image</th>
+                                    <th width="">Action</th>
+                                    <th width=""></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {product.map((product, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{product.product_name}</td>
+                                        <td>{product.cat_name}</td>
+                                        <td>{product.SKU}</td>
+                                        <td>{product.quantity}</td>
+                                        <td>{product.dob}</td>
+                                        {/* <td><img src="{{asset'/products/'.$product->image)}}" style="height:150px; width:150px;"/></td>
 									@if(session('name')  == 'superadmin')
 									<td>
 									<div className="d-flex">
@@ -49,17 +70,16 @@ const Product = () => {
 									</div>
 									</td>
 									@endif
-									<td><a href="{{route('add_sale', $product->product_name)}}" className="btn btn-primary btn-md">Add Sale</a></td>
-								</tr>
-							@endforeach */}
-						  </tbody>
-						</table>
-					</div>	 
-				</div>
-			</div>
-		</div>
+									<td><a href="{{route('add_sale', $product->product_name)}}" className="btn btn-primary btn-md">Add Sale</a></td> */}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-  )
-}
-
-export default Product
+export default Product;
