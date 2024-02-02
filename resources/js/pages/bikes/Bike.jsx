da16c33276738b2fd4a0409f7206e3b642154ab5
Shovickbarua <shovickbarua92@gmail.com>
2024-02-02 22:03:27 +0600

    update

--- resources/js/pages/bikes/Bike.jsx
+++ resources/js/pages/bikes/Bike.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import BikeServices from "../../services/BikeServices";
 
 const Bike = () => {
     const [bike, setBike] = useState([]);
